b9c97f0ae8ee338130e63e19512f2bff7fefa22c
Dr-N0 <beckettjenen@gmail.com>
2019-11-12 16:56:58 -0500

    Added a profanity filter

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+import re
 from flask import Flask, request, jsonify, redirect, make_response, Response
 import json, random, textwrap, requests
 from slackeventsapi import SlackEventAdapter
@@ -46,13 +47,17 @@ def message_actions():
     print(selection)
     if selection == "yes_j":
         global text
+        textParsed = text
+        text = textParsed.replaceAll("<.*?>", "")
         announcement_json = {"ann_body" : text }
         res = requests.post('https://jumpstart.csh.rit.edu/update-announcement', json=announcement_json)
         print(res)
-        print("Interactivity API: " + text)
-        return make_response("Posting right now :)", 200)
+        return make_response("Posting right now :^)", 200)
     elif selection == "no_j":
-        return make_response("Aight bet", 200)
+        return make_response("""My name is Yoshikage Kira. I'm 33 years old. My house is in the northeast section of Morioh, where all the villas are, and I am not married. I work as an employee for the Kame Yu department stores, and I get home every day by 8 PM at the latest. I don't smoke, but I occasionally drink.
+I'm in bed by 11 PM, and make sure I get eight hours of sleep, no matter what. After having a glass of warm milk and doing about twenty minutes of stretches before going to bed, I usually have no problems sleeping until morning. Just like a baby, I wake up without any fatigue or stress in the morning. I was told there were no issues at my last check-up.
+I'm trying to explain that I'm a person who wishes to live a very quiet life. I take care not to trouble myself with any enemies, like winning and losing, that would cause me to lose sleep at night. That is how I deal with society, and I know that is what brings me happiness. Although, if I were to fight I wouldn't lose to anyone.
+""", 200)
     else:
         print("Unknown Response")
 
@@ -65,9 +70,7 @@ def handle_message(event_data):
     
     user = message["user"]
     channel = message["channel"]
-    global text 
     text = message["text"]
-    print("Event API: " + text)
     subtype = message.get("subtype")
 
 
@@ -105,4 +108,9 @@ def handle_message(event_data):
 @slack_events_adapter.on("error")
 def error_handler(err):
     print(f"[ERROR] {str(err)}", file=sys.stderr)
+
+def replacement(toBe):
+    
+
+
 
