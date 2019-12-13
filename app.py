adaf0ee1296e90c9828e0818a545ae47be510f28
Dr-N0 <beckettjenen@gmail.com>
2019-12-13 16:11:34 -0500

    Emoji's show up now, because there's no way to regex it right, also most markdown doesn't show up

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def handle_message(event_data):
         global text
         textpp = re.sub('<.*?>', '', textp, flags=re.IGNORECASE)
         # textppp = re.sub('[:].*?[:]', '', textpp, flags=re.IGNORECASE)
-        text = re.sub('[&]lt;.*?[&]gt;', '', textpp, flags=re.IGNORECASE).replace('*', '').replace('_', '')
+        text = re.sub('[&]lt;.*?[&]gt;', '', textpp, flags=re.IGNORECASE).replace('*', '').replace('_', '').replace('`', '')
         
         # A Dictionary of message attachment options
         attachments_json = [
