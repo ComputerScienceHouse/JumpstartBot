f7488b617e183a02b6dbbeb951ed69cf6d2fdc65
Beckett Jenen <beckettjenen@gmail.com>
2024-04-08 23:15:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -91,8 +91,8 @@ def handle_message(event_data):
             }
         ]
         
-        # if subtype == None:
-        slack_client.chat_postMessage(channel=usernamep, text="Would you like to post this message to Jumpstart?\n\n" + text, attachments=attachments_json)
+        if subtype == None:
+            slack_client.chat_postMessage(channel=usernamep, text="Would you like to post this message to Jumpstart?\n\n" + text, attachments=attachments_json)
 
 # The endpoint Slack will send the user's menu selection to
 @app.route("/slack/message_actions", methods=["POST"])
